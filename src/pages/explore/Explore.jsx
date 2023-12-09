4b71e1aaff295ba6984ac41acedf6b5f7a4bcfe6
Chuboii <favoronyechere@gmail.com>
2023-12-10 00:31:45 +0100

    commit

--- src/pages/explore/Explore.jsx
+++ src/pages/explore/Explore.jsx
@@ -2,15 +2,13 @@ import Thumbnail from '../../components/thumbnails/Thumbnail'
 import FooterMobile from "../../components/footer mobile/FooterMobile"
 import {useEffect, useReducer, useState} from "react"
 import Categories from '../../components/category/Categories'
-import axiosBase from 'axios'
+import axios from 'axios'
 import ThumbnailContainer from '../../components/thumbnail container/ThumbnailContainer'
 import { useNavigate} from 'react-router-dom'
 import SkeletonLoading from '../../components/skeleton/Skeleton'
 import { useDispatch } from 'react-redux'
 
-const axios = axiosBase.create({
-  baseURL: "https://vidtube-ogzw.onrender.com/api/",
-});
+
 
 function Explore() {
   const [videos, setVideos] = useState(null)
@@ -21,7 +19,7 @@ function Explore() {
     dispatch({type:"GET_SEARCH_VALUE", payload:""})
     const getData = async () => {
       try {
-        const res = await axios.get("/video/random")
+        const res = await axios.get("https://vidtube-l48b.onrender.com/api/video/random")
         const data = res.data
 
         setVideos(data)

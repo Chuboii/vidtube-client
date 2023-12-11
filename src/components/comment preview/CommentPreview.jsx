46cdf1f048e4a5046fad7d748167a762a5472684
chuboii <favoronyechere@gmail.com>
2023-12-11 11:28:24 +0100

    fixed some bugs

--- src/components/comment preview/CommentPreview.jsx
+++ src/components/comment preview/CommentPreview.jsx
@@ -1,20 +1,37 @@
 import './CommentPreview.style'
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import {Container, Header, Text, CommentNo, Main, Box, Image, Span} from "../comment preview/CommentPreview.style"
+import {
+  Container,
+  Header,
+  Text,
+  CommentNo,
+  Main,
+  Box,
+  Image,
+  Span
+} from "../comment preview/CommentPreview.style"
 import img from "../../assets/dummy.jpeg"
 import Comments from "../../components/comments/Comments"
-import {useState, useEffect} from "react"
-import { useDispatch, useSelector } from 'react-redux';
+import {
+  useState,
+  useEffect
+} from "react"
+import {
+  useDispatch,
+  useSelector
+} from 'react-redux';
 import axios from 'axios';
-import { useLocation } from 'react-router-dom';
+import {
+  useLocation
+} from 'react-router-dom';
 import CommentForm from '../comment form/CommentForm';
 import DesktopComment from '../desktop comment sec/DesktopComment';
 
-export default function CommentPreview(){
+export default function CommentPreview() {
   const dispatch = useDispatch()
   const location = useLocation()
   const videoId = location.pathname.split('/')[2]
-  const commentPreview = useSelector((state) => state.comment.previewComment)  
+  const commentPreview = useSelector((state) => state.comment.previewComment)
   const allComment = useSelector((state) => state.comment.allComments)
 
   // https://vidtube-l48b.onrender.com
@@ -23,13 +40,17 @@ export default function CommentPreview(){
     const getData = async () => {
       try {
         const data = await axios.get(`https://vidtube-l48b.onrender.com/api/comment/find/${videoId}`, {
-          withCredentials:true
+          withCredentials: true
         })
-        
-        dispatch({type:"GET_ALL_COMMENT", payload: data.data})
+
+        dispatch({
+          type: "GET_ALL_COMMENT", payload: data.data
+        })
       }
       catch (e) {
-        dispatch({type:"COMMENT_ERROR", payload:e.response.data})
+        dispatch({
+          type: "COMMENT_ERROR", payload: e.response.data
+        })
       }
     }
 
@@ -38,53 +59,74 @@ export default function CommentPreview(){
     const getCommentPreview = async () => {
       try {
         const data = await axios.get(`http://localhost:8080/api/comment/previewcomment/${videoId}`, {
-        withCredentials:true
+          withCredentials: true
         })
 
         console.log(data)
 
-        dispatch({type:"GET_COMMENT_PREVIEW", payload:data.data})
+        dispatch({
+          type: "GET_COMMENT_PREVIEW", payload: data.data
+        })
       }
       catch (e) {
-        dispatch({ type: "COMMENT_ERROR", payload: e.response.data })
+        dispatch({
+          type: "COMMENT_ERROR", payload: e.response.data
+        })
       }
     }
     getCommentPreview()
-    
-  },[])
-  
+
+  }, [])
+
 
 
   const enableComment = () => {
-    dispatch({type:"TOGGLE_COMMENT_COMP", payload:true})
-    dispatch({type:"DISABLE_BG", payload:true})
+    dispatch({
+      type: "TOGGLE_COMMENT_COMP", payload: true
+    })
+    dispatch({
+      type: "DISABLE_BG", payload: true
+    })
   }
 
   // const isAavailable = commentPreview.length > 0 ? commentPreview[0].photoURL : ''
-  
+
   return (
     <>
-      
-      <Comments/>
-<Container onClick={enableComment}>
+    {
+
+      allComment && commentPreview ?
+      <>
+      <Comments />
+      <Container onClick={enableComment}>
   <Header>
    <Text> Comments </Text>
-   <CommentNo>{allComment ? allComment.length : 0} </CommentNo>
+   <CommentNo>{allComment && allComment.length > 0 ? allComment.length: 0} </CommentNo>
   </Header>
-  
   <Main>
-               
                   <Box>
-                    <Image src={commentPreview.length && commentPreview> 0 ? commentPreview[0].photoURL : ''} />
-                    <Span>{commentPreview.length > 0 && commentPreview ? commentPreview[0].comment : ''} </Span>
-                
+                    <Image src={commentPreview.length && commentPreview > 0 ? commentPreview[0].photoURL: ''} />
+                    <Span>{commentPreview.length > 0 && commentPreview ? commentPreview[0].comment: ''} </Span>
                   </Box>
-             
-  <KeyboardArrowDownIcon/>
+  <KeyboardArrowDownIcon />
+  </Main>
+          </Container> < /> :    
+          <>
+          <Comments / >
+      <Container onClick={enableComment}>
+  <Header>
+   <Text> Comments </Text>
+
+  </Header>
+  <Main>
+                  <Box>
+                  Post a comment here...
+                  </Box>
+  <KeyboardArrowDownIcon />
   </Main>
           </Container>
-                
-      </>
-      
-    )
+          </>
+    } < / >
+
+  )
 }
--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -1,16 +1,45 @@
 import CloseIcon from '@mui/icons-material/Close';
 import "./Comments.style"
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
-import {Container, Header, H3, Main, Alert, Comment, Image,Form, ProfileImg, Input, Inner, Box, Wrapper, Name, Dot, Time, Text, Span,Icon
+import {
+  Container,
+  Header,
+  H3,
+  Main,
+  Alert,
+  Comment,
+  Image,
+  Form,
+  ProfileImg,
+  Input,
+  Inner,
+  Box,
+  Wrapper,
+  Name,
+  Dot,
+  Time,
+  Text,
+  Span,
+  Icon
 } from "../comments/Comments.style"
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import GlobalStyle from "../../global style/GlobalStyle"
 import CommentForm from "../comment form/CommentForm"
-import {useEffect, useState} from "react"
-import { useDispatch, useSelector } from 'react-redux';
+import {
+  useEffect,
+  useState
+} from "react"
+import {
+  useDispatch,
+  useSelector
+} from 'react-redux';
 import axios from 'axios';
-import {formatDistanceToNow} from 'date-fns'
-import { useLocation } from 'react-router-dom';
+import {
+  formatDistanceToNow
+} from 'date-fns'
+import {
+  useLocation, useNavigate
+} from 'react-router-dom';
 
 export default function Comments() {
   const currentUser = useSelector((state) => state.user.currentUser)
@@ -19,66 +48,86 @@ export default function Comments() {
   const toggleCommentComp = useSelector((state) => state.toggle.toggleCommentComp)
   const disableBgTouch = useSelector((state) => state.toggle.disableBg)
   const dispatch = useDispatch()
-
+  const navigate = useNavigate()
 
   const disableComment = () => {
-    dispatch({ type: "TOGGLE_COMMENT_COMP", payload:false})
-    dispatch({ type: "DISABLE_BG", payload: false })
+    dispatch({
+      type: "TOGGLE_COMMENT_COMP", payload: false
+    })
+    dispatch({
+      type: "DISABLE_BG", payload: false
+    })
   }
 
-  
-  const enableCommentForm = () => dispatch({type:'TOGGLE_COMMENT_FORM', payload:true})
-  const disableCommentForm = () => dispatch({type:'TOGGLE_COMMENT_FORM', payload:false})
-  
+
+  const enableCommentForm = () => {
+    if (currentUser) {
+      dispatch({
+        type: 'TOGGLE_COMMENT_FORM', payload: true
+      })
+    } else {
+      navigate("/signup")
+    }
+  }
+  const disableCommentForm = () => dispatch({
+    type: 'TOGGLE_COMMENT_FORM', payload: false
+  })
+
   return(
     <>
 
-   {disableBgTouch && <GlobalStyle overflow={"hidden"}/>}
-    <Container b={toggleCommentComp ? "0" : "-5000px"} onClick={disableCommentForm}>
+    {
+      disableBgTouch && <GlobalStyle overflow={"hidden"} />
+    } < Container b = {
+      toggleCommentComp ? "0": "-5000px"
+    } onClick = {
+      disableCommentForm
+    } >
     <Header>
     <H3> Comments</H3>
-    <CloseIcon onClick={disableComment}/>
+    <CloseIcon onClick={disableComment} />
     </Header>
     <Main>
     <Alert> Remember to keep comments respectful and to follow our <Inner> Community Guidelines </Inner></Alert>
           {commentData ? commentData.map(data => {
-             const date = new Date(data.createdAt)
-            return (
-              <Comment key={data._id}>
+      const date = new Date(data.createdAt)
+      return (
+        <Comment key={data._id}>
                 <Image src={data.photoURL} />
                 <Box>
                   <Wrapper>
                     <Name> {data.name}</Name>
                     <Dot> . </Dot>
-                    <Time> {formatDistanceToNow(date, { addSuffix: true })} </Time>
+                    <Time> {formatDistanceToNow(date, {
+          addSuffix: true
+        })} </Time>
                   </Wrapper>
                   <Text>
                    {data.comment}
                   </Text>
                   <Icon>
-                    <ThumbUpIcon sx={{ fontSize: "15px" }} />
+                    <ThumbUpIcon sx={ { fontSize: "15px" }} />
                     <Span> 7.5K </Span>
                   </Icon>
                 </Box>
-                <MoreVertIcon sx={{ position: "absolute", right: ".5rem" }} />
+                <MoreVertIcon sx={ { position: "absolute", right: ".5rem" }} />
               </Comment>
-            )
-          }) : ""}
-    
+      )
+    }): ""}
 
         </Main>
-        {currentUser ?
-          <Form>
-            <ProfileImg src={currentUser ? currentUser.img : ""} />
+
+    <Form>
+            <ProfileImg src={currentUser ? currentUser.img: ""} />
             <Input onClick={(e) => {
-              e.stopPropagation()
-              enableCommentForm()
-            }}
-              placeholder="Add a comment..."
-              readOnly />
-          </Form> : ""}
-{toggleCommentForm && <CommentForm/>}
-    </Container>
-    </>
-    )
+      e.stopPropagation()
+      enableCommentForm()
+    }}
+      placeholder="Add a comment..."
+      readOnly />
+          </Form>
+    {
+      toggleCommentForm && <CommentForm />
+    } < /Container> < / >
+  )
 }

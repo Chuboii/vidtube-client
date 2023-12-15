5b2ce55fc43761879d4af6bf7071bc37ea51fcc1
chuboii <favoronyechere@gmail.com>
2023-12-15 01:35:37 +0100

    fixed style issues

--- src/components/description/Description.style.js
+++ src/components/description/Description.style.js
@@ -1,24 +1,33 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  background-color: #222831;
-  color:white;
-  margin-top: 1rem;
-  border-radius: 10px;
-  width: 100%;
-  padding: 1rem;
+background-color: #222831;
+color:white;
+margin-top: 1rem;
+border-radius: 10px;
+width: 100%;
+padding: 1rem;
 `
 
 export const Text = styled.div`
- font-size: 17px;
- margin-top:.4rem;
+font-size: 17px;
+margin-top:.4rem;
+
+@media screen (max-width:768px){
+font-size:13px;
+}
 `
 export const Tags = styled.div`
-  display: flex;
-  font-size:17px ;
-  align-items:center;
+display: flex;
+font-size:17px ;
+align-items:center;
+margin-right:.3rem;
+
+@media screen (max-width:768px){
+font-size:13px;
+}
 `
 
 export const Tag = styled.div`
-  margin-right: .5rem;
+margin-right: .5rem;
 `

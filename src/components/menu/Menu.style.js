2ae6a125b4b167c1cf44574e5b44880f5414d9ac
chuboii <favoronyechere@gmail.com>
2023-12-15 13:57:25 +0100

    commit

--- src/components/menu/Menu.style.js
+++ src/components/menu/Menu.style.js
@@ -1,56 +1,69 @@
-import {styled} from "styled-components"
+import {
+  styled
+} from "styled-components"
 
 export const Container = styled.div`
-    position:fixed;
-    top:1rem;
-    right: 4rem;
+position:fixed;
+top:1rem;
+right: 4rem;
 
-    background:#222831;
-    transition:all .5s;
-    width:300px;
-    border-radius: 10px;
-    color:white;
-    z-index:8000;
+background:#222831;
+transition:all .5s;
+width:300px;
+border-radius: 10px;
+color:white;
+z-index:8000;
 `
 export const Header = styled.div`
-  display:flex;
-  
-  padding: 1rem;
+display:flex;
+
+padding: 1rem;
 `
 export const Box = styled.div`
 display:flex;
-  align-items:center;
-  justify-content: space-between;
-  padding:1rem ;
-  border-bottom:1px solid rgba(255,255,255,.1);
+align-items:center;
+justify-content: space-between;
+padding:1rem ;
+border-bottom:1px solid rgba(255,255,255,.1);
 &:hover{
-  color: red;
+color: red;
 }
 `
 
 export const Span = styled.div`
-   margin-left:1.5rem;
-   font-size:15px;
-   
-  font-size: 18px;
+margin-left:1.5rem;
+font-size:15px;
+
+font-size: 18px;
+
+@media screen and (max-width:768px){
+font-size:14px;
+}
 `
 
 export const Wrapper = styled.div`
-  margin-left: 1rem;
-  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+margin-left: 1rem;
+font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
 `
 export const Name = styled.h4`
 font-size: 19px;
+
+@media screen and (max-width:768px){
+font-size:14px;
+}
 `
 
 export const Username = styled.div`
-  margin-bottom: .5rem;
-  margin-top: .3rem;
+margin-bottom: .5rem;
+margin-top: .3rem;
+
+@media screen and (max-width:768px){
+font-size:12px;
+}
 `
 export const Wrap = styled.div`
-  margin-bottom: .5rem;
-  display: flex;
-  align-items: center;
-  
-`
+margin-bottom: .5rem;
+display: flex;
+align-items: center;
 
+`
